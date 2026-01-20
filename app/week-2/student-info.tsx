3730495cf4b3de61505a4c0fc7c68f5d9a06f99c
yufeng-fan <yufeng.fan@edu.sait.ca>
2026-01-20 14:47:14 -0700

    add week-2

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <main>
+      <p>Name: Yufeng Fan</p>
+      <p>
+        <Link href="https://github.com/yufeng-fan/cprg306-assignments">
+          GitHub repository
+        </Link>
+      </p>
+    </main>
+  );
+}
